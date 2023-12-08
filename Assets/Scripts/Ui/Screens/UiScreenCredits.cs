98d333fe896bae596efd6d8f4055579a0626ab54
AAAAAAAAA <maxmerkury13@gmail.com>
2023-12-09 02:28:27 +0700

    rushhhhh

---
+++ Assets/Scripts/Ui/Screens/UiScreenCredits.cs
@@ -0,0 +1,17 @@
+﻿using Root;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Ui.Screens {
+	public class UiScreenCredits : UiScreen {
+		[SerializeField] private Button _back;
+
+		public override void Init() {
+			_back.onClick.AddListener(HandleBack);
+		}
+
+		private void HandleBack() {
+			Core.UiController.Show(UiScreenType.Main);
+		}
+	}
+}
