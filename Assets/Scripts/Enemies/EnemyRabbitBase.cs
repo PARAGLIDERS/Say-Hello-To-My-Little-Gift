f2009e5f362b46a0037d5b0aa5ee16593cae0c6d
AAAAAAAAA <maxmerkury13@gmail.com>
2023-12-25 03:17:16 +0700

    fix rabbits

--- Assets/Scripts/Enemies/EnemyRabbitBase.cs
+++ Assets/Scripts/Enemies/EnemyRabbitBase.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace Enemies {
 	public abstract class EnemyRabbitBase : Enemy {
--- Assets/Scripts/Enemies/EnemyRabbitClone.cs
+++ Assets/Scripts/Enemies/EnemyRabbitClone.cs
@@ -1,9 +1,19 @@
 ﻿using Root;
+using UnityEngine;
 
 namespace Enemies {
 	public class EnemyRabbitClone : EnemyRabbitBase {
+		public override void Activate(Vector3 position, Quaternion rotation) {
+			base.Activate(position, rotation);
+			_damageable.OnDie += Despawn;
+		}
+
 		public override void Deactivate() {
 			base.Deactivate();
+			_damageable.OnDie -= Despawn;
+		}
+
+		public void Despawn() {
 			Core.PoolController.Spawn(PoolSystem.PoolType.VFX_EnemySpawnEffect, transform.position, transform.rotation);
 		}
 	}
