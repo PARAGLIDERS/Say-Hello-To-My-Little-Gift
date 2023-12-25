ccfcdc0c858e8b6fde8d9b8ee7981a3d76fa06a5
AAAAAAAAA <maxmerkury13@gmail.com>
2023-12-25 11:59:09 +0700

    ui polishing

---
+++ Assets/Scripts/Animations/BounsingUi.cs
@@ -0,0 +1,17 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+namespace Animations {
+	public class BounsingUi : MonoBehaviour{
+		[SerializeField] private float _magnitude = 1.05f;
+		[SerializeField] private float _period = 1;
+
+		private void Awake() {
+			transform
+				.DOScale(_magnitude, _period)
+				.SetEase(Ease.InOutSine)
+				.SetLoops(-1, LoopType.Yoyo)
+				.SetUpdate(true);
+		}
+	}
+}
--- Assets/Scripts/Ui/Screens/UiScreenLoading.cs
+++ Assets/Scripts/Ui/Screens/UiScreenLoading.cs
@@ -5,10 +5,16 @@ using UnityEngine.UI;
 namespace Ui.Screens {
 	public class UiScreenLoading : UiScreen {
 		[SerializeField] private Slider _slider;
+		[SerializeField] private RectTransform _icon;
 
 		public override void Init() {
 			_slider.value = 0f;
 			_slider.DOValue(1f, 1f).SetUpdate(true);
+			_icon
+				.DOScale(1.05f, .1f)
+				.SetEase(Ease.InOutSine)
+				.SetLoops(-1, LoopType.Yoyo)
+				.SetUpdate(true);
 		}
 
         private void OnDestroy() {
