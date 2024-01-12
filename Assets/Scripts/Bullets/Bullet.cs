8e84583cef9a2fa17e63f79a07c073cc48c84bb3
Max Merkuryev <89397351+MaxMerkuryev@users.noreply.github.com>
2024-01-12 19:38:17 +0700

    Update Bullet.cs
    fix git formatting

--- Assets/Scripts/Bullets/Bullet.cs
+++ Assets/Scripts/Bullets/Bullet.cs
@@ -2,18 +2,18 @@ using UnityEngine;
 
 namespace Bullets {
 	public class Bullet : BulletBase {
-        [SerializeField] private BulletConfig _config;
+	        [SerializeField] private BulletConfig _config;
 		[SerializeField] private TrailRenderer _trail;
-
+	
 		private void Awake() {
-            Init(_config);
+	            Init(_config);
 		}
-
+	
 		public override void Activate(Vector3 position, Quaternion rotation) {
-            if(_trail != null) _trail.Clear();
-            base.Activate(position, rotation);            
-            _rigidbody.AddForce(Random.Range(_config.SpeedMin, _config.SpeedMax) * transform.forward, ForceMode.Impulse);
-        }
+	            if(_trail != null) _trail.Clear();
+	            base.Activate(position, rotation);            
+	            _rigidbody.AddForce(Random.Range(_config.SpeedMin, _config.SpeedMax) * transform.forward, ForceMode.Impulse);
+	        }
 
 		protected override void OnTriggerEnter(Collider other) {
 			base.OnTriggerEnter(other);
